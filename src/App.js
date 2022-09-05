65ef2740b1e04a61191bff41daa55770f172b4ac
jacksonohallo <jacksonohallo@gmail.com>
2022-09-05 19:29:02 +0300

    add NavBar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Home from './Components/Home';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Home />
+      <NavBar/>
+      
       
     </div>
   );
---
+++ src/Components/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <NavLink to="/">
+          <button className="">Home</button>
+        </NavLink>
+        <NavLink to="/hotels">
+          <button className="">Hotels</button>
+        </NavLink>
+        <NavLink to="/formpage">
+          <button className="">Add New Hotels</button>
+        </NavLink>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
