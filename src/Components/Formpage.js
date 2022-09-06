24cf7765576752a9ee7068cdc6ca9091dd307117
jacksonohallo <jacksonohallo@gmail.com>
2022-09-06 08:59:40 +0300

    create formpage to enable adding hotels

--- src/Components/Formpage.js
+++ src/Components/Formpage.js
@@ -15,12 +15,57 @@ const FormPage = ({ addHotels }) => {
   };
   function handleSubmit(e) {
     e.preventDefault();
-    const addNewBook = {
+    const addNewHotel = {
       name: newHotel.name,
       description: newHotel.description,
       likes: 0,
     };
 
-  
+    fetch(`http://localhost:9292/hotels`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(addNewHotel),
+    })
+      .then((response) => response.json())
+      .then(addHotels);
+    setNewHotel({
+      name: "",
+      description: "",
+      likes: 0,
+    });
+    history.push("/hotels");
+  }
+  return (
+    <div>
+      <form className="form" onSubmit={handleSubmit}>
+        <h4>Add new hotels here: </h4>
+
+        <input
+          type="text"
+          placeholder="Hotel name"
+          name="title"
+          value={newHotel.name}
+          onChange={handleChange}
+        ></input>
+        <br />
+        <input
+          type="text"
+          placeholder="Description"
+          name="description"
+          value={newHotel.description}
+          onChange={handleChange}
+        ></input>
+        <br />
+
+        <input className="button" type="submit" />
+      </form>
+      <img
+        alt="hotel"
+        src="https://images.unsplash.com/photo-1481627834876-b7833e8f5570?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fGJvb2tzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+      ></img>
+    </div>
+  );
 };
 export default FormPage;
