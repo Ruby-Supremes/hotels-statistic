52bc46bbd093ace8ad09c28fbdbb2e2052807320
jacksonohallo <jacksonohallo@gmail.com>
2022-09-06 09:49:51 +0300

    add api with id

--- src/Components/AddReview.js
+++ src/Components/AddReview.js
@@ -11,6 +11,30 @@ const AddReview = ({ addReview, hotel}) => {
 
     setNewReview({ ...newReview, [name]: value });
   };
+  function handleSubmit(e) {
+    e.preventDefault();
 
+    const addNewReview = {
+      text: newReview.text,
+      book_id: newReview.hotel_id,
+    };
+
+    fetch(`http://localhost:9292/hotels/${hotel.id}/reviews`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(addNewReview),
+    })
+      .then((response) => response.json())
+      .then((data) => addReview(data));
+    setNewReview({
+      text: "",
+      hotel_id: newReview.hotel_id,
+    });
+  }
+  return (
+    
+  );
 };
 export default AddReview;
