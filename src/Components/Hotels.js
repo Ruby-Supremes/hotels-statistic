2840aab416d3abbeee945db0392245b13cc518ff
jacksonohallo <jacksonohallo@gmail.com>
2022-09-06 13:02:08 +0300

    add hotels list

--- src/Components/Hotels.js
+++ src/Components/Hotels.js
@@ -1,18 +1,37 @@
-import EachHotel from "./EachHotel";
+import EachBook from "./EachBook";
 import SearchBar from "./SearchBar";
 
+const Hotels = ({
+  hotelsToDisplay,
+  addReview,
 
-const Hotels = () => { 
+  searchTerm,
+  onSearch,
+  handleUpdateLikes,
+  handleDeleteReview,
+  reviews,
+}) => {
+  const hotelList = hotelsToDisplay.map((hotel) => (
+    <EachBook
+      key={hotel.id}
+      book={hotel}
+      addReview={addReview}
+      handleUpdateLikes={handleUpdateLikes}
+      handleDeleteReview={handleDeleteReview}
+      reviews={reviews}
+    />
+  ));
 
-
-
-
-
-
-
-
-
-}
-
-
+  return (
+    <div>
+      <h6>Search here by hotel name to find a hotel you are looking for: </h6>
+      <SearchBar searchTerm={searchTerm} onSearch={onSearch} />
+      <u>
+        <h1>hotel List</h1>
+      </u>
+      <br />
+      {hotelList}
+    </div>
+  );
+};
 export default Hotels;
